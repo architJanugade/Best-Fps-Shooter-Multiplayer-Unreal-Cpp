16af18ae33403031df59d2daaba8c5aa3ab225c6
architInfiVR <janugadearchit.shivratna@gmail.com>
2023-08-23 19:31:11 +0530

    Project Setup

---
+++ BestFpsShooter/Source/BestFpsShooter/BestFpsShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BestFpsShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BESTFPSSHOOTER_API ABestFpsShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
